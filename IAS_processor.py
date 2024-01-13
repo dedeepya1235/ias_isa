774bf606687556bf57944a2fcc57c81b4ccc355c
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-01-13 15:17:10 +0530

    processor changes

--- IAS_processor.py
+++ IAS_processor.py
@@ -2,24 +2,41 @@
 class IAS_processor:
     
     def __init__(self):
-        self.PC  = 0b000000000000 # Program counter - 12bits
-        self.AC  = 0b0000000000000000000000000000000000000000 # Accumulator - 40bits
-        self.IR  = 0b00000000 # Instruction register - 8bits
-        self.MAR = 0b000000000000 # Memory address register - 12bits
-        self.MBR = 0b0000000000000000000000000000000000000000 # Memory buffer register - 40bits
-        self.IBR = 0b00000000000000000000 # Instruction buffer register - 20bits
-        self.MQ  = 0b0000000000000000000000000000000000000000 # Multiplier/Quotient - 40bits
+        
+        self.PC  = 0b0000_0000_0000 # Program counter - 12bits
+        self.AC  = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000 # Accumulator - 40bits
+        self.IR  = 0b0000_0000 # Instruction register - 8bits
+        self.MAR = 0b0000_0000_0000 # Memory address register - 12bits
+        self.MBR = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000 # Memory buffer register - 40bits
+        self.IBR = 0b0000_0000_0000_0000_0000 # Instruction buffer register - 20bits
+        self.MQ  = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000 # Multiplier/Quotient - 40bits
         self.M   = {} # Main memory with 12bit address keys and 40bit values
     
     def fetch_cycle(self):
+        
         self.MAR = self.PC
         self.MBR = self.M[self.MAR]
-        left_instruction  = self.MBR >> 20
-        right_instruction = self.MBR & 0b11111111111111111111
-        self.IR = left_instruction >> 12
-        self.MAR = left_instruction & 0b111111111111
-        self.IBR = right_instruction
+        
         self.pc += 1
     
     def decode_cycle(self):
         
+        left_instruction  = self.MBR >> 20 # Gettting the first 20bits for left instruction from MBR
+        right_instruction = self.MBR & 0b1111_1111_1111_1111_1111 # Getting the last 20bits for right instruction from MBR
+        
+        self.IR = left_instruction >> 12 # Getting the first 8bits ie opcode
+        self.MAR = left_instruction & 0b1111_1111_1111 # Getting the last 12bits for adress
+        self.IBR = right_instruction # Storing right instruction in IBR
+        
+        self.MBR = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000 # resetting MBR after it's use
+    
+    def execute_cycle(self):
+        
+        if self.IR == 0b0000_1010:
+            pass # implement LOAD MQ
+        elif self.IR == 0b0000_1001:
+            pass # implement LOAD MQ,M(X)
+        elif self.IR == 0b0010_0001:
+            pass # implement STOR M(X)
+        elif self.IR == 0b0000_0001:
+            pass # implement LOAD M(X)
