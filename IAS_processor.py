8fcb3dafc666c0fad8c2c02ec7d715afaed67e05
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-01-13 15:40:17 +0530

    structure of execute cycle

--- IAS_processor.py
+++ IAS_processor.py
@@ -31,12 +31,71 @@ class IAS_processor:
         self.MBR = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000 # resetting MBR after it's use
     
     def execute_cycle(self):
-        
+    
+        # Data transfer instructions
         if self.IR == 0b0000_1010:
             pass # implement LOAD MQ
+        
         elif self.IR == 0b0000_1001:
             pass # implement LOAD MQ,M(X)
+        
         elif self.IR == 0b0010_0001:
             pass # implement STOR M(X)
+        
         elif self.IR == 0b0000_0001:
             pass # implement LOAD M(X)
+        
+        elif self.IR == 0b0000_0010:
+            pass # implement LOAD -M(X)
+        
+        elif self.IR == 0b0000_0011:
+            pass # implement LOAD |M(X)|
+        
+        elif self.IR == 0b0000_0100:
+            pass # implement LOAD -|M(X)|
+        
+        # Unconditional branch instructions
+        elif self.IR == 0b0000_1101:
+            pass # implement JUMP M(X,0:19)
+        
+        elif self.IR == 0b0000_1110:
+            pass # implement JUMP M(X,20:39)
+        
+        # Conditional branch instructions
+        elif self.IR == 0b0000_1111:
+            pass # implement JUMP+ M(X,0:19)
+        
+        elif self.IR == 0b0001_0000:
+            pass # implement JUMP+ M(X,20:39)
+        
+        # Arithmetic instructions
+        elif self.IR == 0b0000_0101:
+            pass # implement ADD M(X)
+        
+        elif self.IR == 0b0000_0111:
+            pass # implement ADD |M(X)|
+        
+        elif self.IR == 0b0000_0110:
+            pass # implement SUB M(X)
+        
+        elif self.IR == 0b0000_1000:
+            pass # implement SUB |M(X)|
+        
+        elif self.IR == 0b0000_1011:
+            pass # implement MUL M(X)
+        
+        elif self.IR == 0b0000_1100:
+            pass # implement DIV M(X)
+        
+        elif self.IR == 0b0001_0100:
+            pass # implement LSH
+        
+        elif self.IR == 0b0001_0101:
+            pass # implement RSH
+        
+        # Adress modify instructions
+        elif self.IR == 0b0001_0010:
+            pass # implement STOR M(X,8:19)
+        
+        elif self.IR == 0b0001_0011:
+            pass # implement STOR M(X,28:39)
